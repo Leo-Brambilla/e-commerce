129c572110b81fe4567ab2d773858c76d9e42e18
Leo-Brambilla <leo.a.brambilla@gmail.com>
2023-04-09 20:45:56 -0300

    Atualização de CRUD Usuarios e criação de usuarios fictícios no arquivo PopulateService

--- e-commerce/src/main/java/com/leonardobrambilla/ecommerce/ecommerce/repositories/UsuarioRepository.java
+++ e-commerce/src/main/java/com/leonardobrambilla/ecommerce/ecommerce/repositories/UsuarioRepository.java
@@ -2,10 +2,11 @@ package com.leonardobrambilla.ecommerce.ecommerce.repositories;
 
 import com.leonardobrambilla.ecommerce.ecommerce.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>, JpaSpecificationExecutor<Usuario> {
     Optional<Usuario> findAllById(Integer id);
 }
